7b138d285e465f774a28edd2b1d1ba38d0dd02a3
Speuce <mrk5199@gmail.com>
2023-02-04 00:03:41 -0600

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,9 +2,21 @@ import { fileURLToPath, URL } from "node:url";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    manifest: true,
+    outDir: resolve("./dist"),
+    emptyOutDir: true,
+    sourcemap: true,
+    rollupOptions: {
+      input: {
+        main: resolve("src/main.js"),
+      },
+    },
+  },
   plugins: [vue()],
   server: {
     port: 3000,
@@ -14,6 +26,7 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
+  base: "/",
   test: {
     environment: "jsdom",
   },
