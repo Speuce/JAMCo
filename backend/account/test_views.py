d9a120fd82cbf836e4b99a448768b320849cd064
Speuce <mrk5199@gmail.com>
2023-02-04 09:48:12 -0600

    Create account view-layer tests

---
+++ backend/account/test_views.py
@@ -0,0 +1,18 @@
+from django.test import RequestFactory, TestCase
+from django.http import JsonResponse
+from .views import get_or_create_account
+
+
+class GetOrCreateAccountTests(TestCase):
+
+    def setUp(self):
+        self.factory = RequestFactory()
+
+    def test_get_or_create_account(self):
+        request = self.factory.post('/get_or_create_account',
+                                    data='{"credential": "test_credential"}',
+                                    content_type='application/json')
+        response = get_or_create_account(request)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content, b'{"data": "account_created"}')
+        self.assertIsInstance(response, JsonResponse)
