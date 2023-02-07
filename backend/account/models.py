ac69aef66cb33653af34fd817ac9b16670b4ccb1
Alex Kitt <alex.kitt@outlook.com>
2023-02-07 12:05:01 -0600

    Add all fields except for location

--- backend/account/models.py
+++ backend/account/models.py
@@ -2,4 +2,9 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    pass#google_id = models.field
+    # Name, username, and email are included with AbstractUser
+    google_id = models.TextField()
+    image_url = models.TextField()
+    #location = models. todo: Import django-cities
+    birthday = models.DateField(verbose_name='Date of Birth')
+    field_of_work = models.CharField(max_length=30)
