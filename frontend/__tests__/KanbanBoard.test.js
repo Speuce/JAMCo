625ec0149833cc4865cbc9eba93f3ebacedebd7d
Matthew Kwiatkowski <mrk5199@gmail.com>
2023-02-11 11:36:49 -0600

    Update KanbanBoard.test.js

--- frontend/__tests__/KanbanBoard.test.js
+++ frontend/__tests__/KanbanBoard.test.js
@@ -24,19 +24,4 @@ describe('KanbanBoard', () => {
     let columns = wrapper.findAllComponents(VueDraggableNext)
     expect(columns.length).toBe(Object.keys(testColumnMapping).length)
   })
-
-  it('updates the column of a job when it is moved', () => {
-    let column = wrapper.findAllComponents(VueDraggableNext)
-    let job = wrapper.findComponent(JobCard)
-    expect(job.vm.job.columnId).toBe(1)
-
-    column[1].vm.$emit('change', { added: { element: job.vm.job } }, 2)
-    expect(job.vm.job.columnId).toBe(2)
-  })
-
-  it('emits showDetailModal when card clicked', () => {
-    let card = wrapper.findComponent(JobCard)
-    card.trigger('click')
-    expect(wrapper.emitted('showDetailModal')).toBeTruthy()
-  })
 })
