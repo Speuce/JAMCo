e6c61ce47325d518427283e9dde61135313b601d
LokiFrostGiant <colinjohnson@mymts.net>
2023-02-14 22:01:15 -0600

    add job create query

--- backend/job/query.py
+++ backend/job/query.py
@@ -4,4 +4,16 @@ Job queries
 Query functions for job related operations.
 """
 
+from django.core.exceptions import ObjectDoesNotExist
 from .models import *
+
+
+def create_job(payload: dict) -> Job:
+    try:
+        return Job.objects.get(id=payload["id"])
+    except ObjectDoesNotExist:
+        return None  # what to put here?
+
+
+def get_all_jobs() -> list(Job):
+    return None
