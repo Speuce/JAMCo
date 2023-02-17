9d4d5f86c80499064fca15338c3654d5668af6f4
Speuce <mrk5199@gmail.com>
2023-02-17 09:00:10 -0600

    Update return of jobs in views

--- backend/job/views.py
+++ backend/job/views.py
@@ -12,7 +12,6 @@ from django.core.serializers import serialize
 from jamco.helper import read_request
 from . import business
 
-
 logger = logging.getLogger(__name__)
 
 
@@ -31,8 +30,9 @@ def get_minimum_jobs(request: HttpRequest):
 
     try:
         jobs = list(business.get_minimum_jobs(user_id))
-        return JsonResponse(status=200, data={'jobs':json.dumps(jobs)})
+        return JsonResponse(status=200, data={'jobs': jobs})
     except Exception as err_msg:
+        logger.error(err_msg)
         return JsonResponse(status=400, data={"error": repr(err_msg)})
 
 
