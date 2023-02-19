c0fe9ec3dd05b7e1d275b66422e0fd097e88dbe1
Speuce <mrk5199@gmail.com>
2023-02-19 13:14:07 -0600

    Add theme and colours to vuetify and colours.css

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -20,6 +20,32 @@ const vuetify = createVuetify({
   icons: {
     defaultSet: 'mdi',
   },
+  theme: {
+    options: {
+      customProperties: true,
+    },
+    defaultTheme: 'jamcoLightTheme',
+    themes: {
+      jamcoLightTheme: {
+        dark: false,
+        colors: {
+          background: '#FFFFFF',
+          surface: '#FFFFFF',
+          primary: '#b996ff',
+          'primary-darken-1': '#3700B3',
+          secondary: '#03DAC6',
+          'secondary-darken-1': '#018786',
+          error: '#B00020',
+          info: '#2196F3',
+          success: '#4CAF50',
+          warning: '#FB8C00',
+          greyback: '#f7fafc',
+          greytext: '#4a5568',
+        },
+        variables: {},
+      },
+    },
+  },
 })
 
 const app = createApp(App).use(vuetify)
