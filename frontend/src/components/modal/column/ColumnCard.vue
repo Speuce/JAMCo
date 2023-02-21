507d4fa4538de3ecbee3704c2a9efde55bb0e109
Jared Webber <jaredwebberdev@gmail.com>
2023-02-21 09:28:12 -0600

    dont disable draggable btn

--- frontend/src/components/modal/column/ColumnCard.vue
+++ frontend/src/components/modal/column/ColumnCard.vue
@@ -2,7 +2,7 @@
   <div class="col-card">
     <v-row>
       <v-col cols="12" sm="2">
-        <v-btn icon flat disabled>
+        <v-btn icon flat>
           <v-icon size="large" color="grey">
             mdi-drag-horizontal-variant
           </v-icon>
