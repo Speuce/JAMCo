eea42ae429fd102ea95e92dd26a878f65563e0ad
Jared Webber <jaredwebberdev@gmail.com>
2023-03-01 09:12:21 -0600

    validate db

--- backend/tests/integration/test_account.py
+++ backend/tests/integration/test_account.py
@@ -1,9 +1,10 @@
 import logging
 import json
-from django.test import TestCase, TransactionTestCase
+from django.test import TransactionTestCase
 from django.urls import reverse
 from unittest import mock
 from django.http.cookie import SimpleCookie
+from account.models import User
 
 logger = logging.getLogger(__name__)
 
@@ -31,31 +32,41 @@ class GetOrCreateAccountTests(TransactionTestCase):
             content_type="application/json",
             **self.header,
         )
+        user_data = {
+            "id": 1,
+            "google_id": "unique_user_id",
+            "username": 0,
+            "image_url": None,
+            "last_name": "",
+            "birthday": None,
+            "city": None,
+            "country": None,
+            "region": None,
+            "email": "useremail",
+            "field_of_work": None,
+            "first_name": "firstname",
+            "last_login": None,
+        }
         self.assertEqual(response.status_code, 200)
         # The query should return the User object. Since a mock is used,
         # the id is as given
         self.assertEqual(
             json.loads(response.content)["data"],
-            {
-                "id": 1,
-                "google_id": "unique_user_id",
-                "username": 0,
-                "image_url": None,
-                "last_name": "",
-                "birthday": None,
-                "city": None,
-                "country": None,
-                "region": None,
-                "email": "useremail",
-                "field_of_work": None,
-                "first_name": "firstname",
-                "last_login": None,
-            },
+            user_data,
+        )
+
+        user_data["username"] = "0"
+        self.assertEqual(len(User.objects.all()), 1)
+        self.assertEqual(
+            User.objects.get(google_id="unique_user_id").to_dict(),
+            user_data,
         )
 
 
 @mock.patch("google.oauth2.id_token.verify_oauth2_token")
-class UpdateAccountTests(TestCase):
+class UpdateAccountTests(TransactionTestCase):
+    reset_sequences = True
+
     def setUp(self):
         self.client.cookies = SimpleCookie({"csrftoken": "valid_csrf_token"})
         self.header = {
@@ -77,7 +88,26 @@ class UpdateAccountTests(TestCase):
             **self.header,
         )
 
-        logger.debug(f"here!: {user_resp.json()}")
+        self.assertEqual(len(User.objects.all()), 1)
+        self.assertEqual(
+            User.objects.get(google_id="unique_user_id").to_dict(),
+            {
+                "birthday": None,
+                "city": None,
+                "country": None,
+                "email": "useremail",
+                "field_of_work": None,
+                "first_name": "firstname",
+                "google_id": "unique_user_id",
+                "id": 1,
+                "image_url": None,
+                "last_login": None,
+                "last_name": "",
+                "region": None,
+                "username": "0",
+            },
+        )
+
         # Try updating it, the request should succeed
         response = self.client.post(
             reverse("update_account"),
@@ -85,6 +115,26 @@ class UpdateAccountTests(TestCase):
             content_type="application/json",
         )
         self.assertEqual(response.status_code, 200)
+
+        self.assertEqual(len(User.objects.all()), 1)
+        self.assertEqual(
+            User.objects.get(google_id="unique_user_id").to_dict(),
+            {
+                "birthday": None,
+                "city": None,
+                "country": None,
+                "email": "useremail",
+                "field_of_work": None,
+                "first_name": "Rob",
+                "google_id": "unique_user_id",
+                "id": 1,
+                "image_url": None,
+                "last_login": None,
+                "last_name": "",
+                "region": None,
+                "username": "0",
+            },
+        )
 
     def test_invalid_account_update(self, mock_verify_oauth2_token):
         mock_verify_oauth2_token.return_value = {
@@ -101,6 +151,26 @@ class UpdateAccountTests(TestCase):
         )
         self.assertEqual(response.status_code, 200)
 
+        self.assertEqual(len(User.objects.all()), 1)
+        self.assertEqual(
+            User.objects.get(google_id="unique_user_id").to_dict(),
+            {
+                "birthday": None,
+                "city": None,
+                "country": None,
+                "email": "useremail",
+                "field_of_work": None,
+                "first_name": "firstname",
+                "google_id": "unique_user_id",
+                "id": 1,
+                "image_url": None,
+                "last_login": None,
+                "last_name": "",
+                "region": None,
+                "username": "0",
+            },
+        )
+
         # Should fail if the given fields don't exist
         response = self.client.post(
             reverse("update_account"),
@@ -109,9 +179,31 @@ class UpdateAccountTests(TestCase):
         )
         self.assertEqual(response.status_code, 400)
 
+        self.assertEqual(len(User.objects.all()), 1)
+        self.assertEqual(
+            User.objects.get(google_id="unique_user_id").to_dict(),
+            {
+                "birthday": None,
+                "city": None,
+                "country": None,
+                "email": "useremail",
+                "field_of_work": None,
+                "first_name": "firstname",
+                "google_id": "unique_user_id",
+                "id": 1,
+                "image_url": None,
+                "last_login": None,
+                "last_name": "",
+                "region": None,
+                "username": "0",
+            },
+        )
+
 
 @mock.patch("google.oauth2.id_token.verify_oauth2_token")
-class AccountTestCase(TestCase):
+class AccountTestCase(TransactionTestCase):
+    reset_sequences = True
+
     def setUp(self):
         self.client.cookies = SimpleCookie({"csrftoken": "valid_csrf_token"})
         self.header = {
@@ -128,6 +220,7 @@ class AccountTestCase(TestCase):
             **self.header,
         )
         self.assertEqual(response.status_code, 401)
+        self.assertEqual(len(User.objects.all()), 0)
 
         # No CSRF in Cookie
         self.client.cookies = SimpleCookie({"not_a_csrftoken": "not_the_droid"})
@@ -143,6 +236,7 @@ class AccountTestCase(TestCase):
             **self.header,
         )
         self.assertEqual(response.status_code, 401)
+        self.assertEqual(len(User.objects.all()), 0)
 
         # No CSRF in Header
         self.client.cookies = SimpleCookie({"csrftoken": "valid_csrf_token"})
@@ -158,6 +252,7 @@ class AccountTestCase(TestCase):
             **self.header,
         )
         self.assertEqual(response.status_code, 401)
+        self.assertEqual(len(User.objects.all()), 0)
 
         # CSRFs do not match
         self.client.cookies = SimpleCookie({"csrftoken": "one_csrf_token"})
@@ -173,3 +268,4 @@ class AccountTestCase(TestCase):
             **self.header,
         )
         self.assertEqual(response.status_code, 401)
+        self.assertEqual(len(User.objects.all()), 0)
