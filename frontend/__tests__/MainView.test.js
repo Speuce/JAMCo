ac0f7de36a771606bb1b8214c3793fb54a9b4f38
Speuce <mrk5199@gmail.com>
2023-03-04 23:40:16 -0600

    Update MainView.test.js

--- frontend/__tests__/MainView.test.js
+++ frontend/__tests__/MainView.test.js
@@ -1,26 +1,30 @@
 import { flushPromises, shallowMount } from '@vue/test-utils'
 import MainView from '@/views/MainView.vue'
-import { expect, describe, it, vi, afterEach } from 'vitest'
+import { expect, describe, it, vi, afterEach, beforeEach } from 'vitest'
 import { postRequest } from '@/helpers/requests.js'
+import { getAuthToken } from '@/helpers/auth-cookie.js'
 
 vi.mock('@/helpers/requests.js', () => ({
   postRequest: vi.fn(),
 }))
 
-function initAuthMocks() {
-  vi.mock('@/helpers/auth-cookie.js', () => ({
-    getAuthToken: vi.fn(),
-    setAuthToken: vi.fn(),
-  }))
+vi.mock('@/helpers/auth-cookie.js', () => ({
+  getAuthToken: vi.fn(),
+  setAuthToken: vi.fn(),
+}))
+
+const mostPostRequest = (url) => {
+  return Promise.resolve({ data: {}, user: 'gerald' })
 }
 
 describe('MainView', () => {
   afterEach(() => {
-    vi.resetAllMocks()
+    vi.restoreAllMocks()
   })
-
+  beforeEach(() => {
+    postRequest.mockImplementation(mostPostRequest)
+  })
   it('sets userData when userSignedIn is called', () => {
-    initAuthMocks()
     const wrapper = shallowMount(MainView)
     const resp = { data: { id: -1 }, token: 'new_token' }
     wrapper.vm.userSignedIn(resp)
@@ -28,7 +32,6 @@ describe('MainView', () => {
   })
 
   it('opens setupModal when a required field is not filled', () => {
-    initAuthMocks()
     const wrapper = shallowMount(MainView)
     const resp = {
       // no email present
@@ -46,7 +49,6 @@ describe('MainView', () => {
   })
 
   it('posts userData to update_account, closes setupModal on updateUserAccount call', async () => {
-    initAuthMocks()
     const wrapper = shallowMount(MainView)
     expect(wrapper.vm.setupModalVisible).toEqual(false)
     const resp = { data: { id: -1 }, token: 'new_token' }
@@ -64,19 +66,13 @@ describe('MainView', () => {
   })
 
   it('authenticates token when cookie found', async () => {
-    vi.mock('@/helpers/auth-cookie.js', () => ({
-      getAuthToken: vi.fn().mockReturnValue('bruh'),
-      setAuthToken: vi.fn(),
-    }))
+    getAuthToken.mockReturnValue('bruh')
     const wrapper = shallowMount(MainView)
-
     flushPromises()
 
     expect(postRequest).toHaveBeenCalledWith(
       'account/api/validate_auth_token',
-      {
-        token: 'token',
-      },
+      'bruh',
     )
     expect(wrapper.vm.userSignedIn).toBeCalledWith({
       data: { id: 0 },
