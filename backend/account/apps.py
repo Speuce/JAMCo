82454df718504eee63e2598efde720159d62cf7d
Speuce <mrk5199@gmail.com>
2023-03-07 21:31:53 -0600

    Add hook to delete test user on init

--- backend/account/apps.py
+++ backend/account/apps.py
@@ -1,6 +1,13 @@
 from django.apps import AppConfig
+from django.conf import settings
 
 
 class AccountConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "account"
+
+    def ready(self):
+        from .models import User
+
+        if settings.IS_TEST:
+            User.objects.filter(google_id="1234567890").delete()
