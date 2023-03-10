ce6a6c43e8c3d9a474c910c02b4d7c241a53820d
Alex Kitt <alex.kitt@outlook.com>
2023-03-09 21:40:25 -0600

    Streamline factory calls

--- backend/job/tests/test_views.py
+++ backend/job/tests/test_views.py
@@ -137,16 +137,11 @@ class TestViews(TestCase):
 
 @patch("job.business.create_review_request")
 class ReviewRequestTests(TestCase):
-    def setUp(self):
-        self.factory = RequestFactory()
-
     def test_create_review_request(self, mock_create_review_request):
-        job = JobFactory()
-
         # Prepare data
-        review_request_data = {"job_id": job.id, "message": "i showed you my cover letter please respond"}
+        review_request_data = {"job_id": JobFactory().id, "message": "i showed you my cover letter please respond"}
         request_body = json.dumps(review_request_data).encode("utf-8")
-        request = self.factory.post("/create_review_request/", data=request_body, content_type="application/json")
+        request = RequestFactory().post("/create_review_request/", data=request_body, content_type="application/json")
 
         # Set up mock
         review_request = MagicMock(to_dict=MagicMock(return_value={**review_request_data, "id": 1}))
@@ -161,7 +156,7 @@ class ReviewRequestTests(TestCase):
 
     def test_create_review_request_with_error(self, mock_create_review_request):
         request_body = json.dumps({}).encode("utf-8")
-        request = self.factory.post("/create_review_request/", data=request_body, content_type="application/json")
+        request = RequestFactory().post("/create_review_request/", data=request_body, content_type="application/json")
 
         # Set up mock
         mock_create_review_request.side_effect = Exception("Something went wrong!")
