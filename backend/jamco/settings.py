cafcbf42b563c55a4b8ffcee54fdd88480254b51
Speuce <mrk5199@gmail.com>
2023-03-11 17:54:51 -0600

    Update settings.py

--- backend/jamco/settings.py
+++ backend/jamco/settings.py
@@ -27,6 +27,8 @@ SECURE_CROSS_ORIGIN_OPENER_POLICY = "same-origin-allow-popups"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = not os.getenv("PROD", 0)
 IS_TEST = os.getenv("TEST", 1)
+PROD = os.getenv("PROD", 0)
+printf("--------------- PROD: %s", PROD)
 
 SETTINGS_PATH = os.path.dirname(os.path.dirname(__file__))
 
