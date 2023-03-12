ecf20aaae7aa5ed462afba80ef280ae1edb1d2ac
Matthew Kwiatkowski <mrk5199@gmail.com>
2023-03-11 20:06:10 -0600

    Update views.py

--- backend/jamco/views.py
+++ backend/jamco/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.views.decorators.csrf import ensure_csrf_cookie
-
+from django.conf import settings
 
 @ensure_csrf_cookie
 def index(request):
-    return render(request, "index.html")
+    if settings.PROD:
+        return render(request, "index.html")
+    else:
+       return render(request, "index-dev.html")     
