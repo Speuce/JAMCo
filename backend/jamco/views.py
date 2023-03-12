33ab0e8f27450927c437ad4cafcda08446b45ca6
Matthew Kwiatkowski <mrk5199@gmail.com>
2023-03-11 21:17:56 -0600

    Fix linting in views.py

--- backend/jamco/views.py
+++ backend/jamco/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.views.decorators.csrf import ensure_csrf_cookie
-from django.conf import settings
-import os
+
 
 @ensure_csrf_cookie
 def index(request):
