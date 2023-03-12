a086833a119a361bac0f1946286aa24e7cdaad5b
Matthew Kwiatkowski <mrk5199@gmail.com>
2023-03-11 20:28:17 -0600

    Update views.py

--- backend/jamco/views.py
+++ backend/jamco/views.py
@@ -5,8 +5,8 @@ import os
 
 @ensure_csrf_cookie
 def index(request):
-    if os.getenv("PROD"):
+    if os.getenv("PROD2"):
 #         return render(request, "index.html")
-       raise ValueError("prod is true")
+       raise ValueError("prod is true: " + str(os.getenv("PROD2",0)))
     else:
        return render(request, "index-dev.html")     
