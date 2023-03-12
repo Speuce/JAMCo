324bcfffba03e1db8e5544f30b3f8e0b8f23f9ac
Matthew Kwiatkowski <mrk5199@gmail.com>
2023-03-11 20:42:09 -0600

    Update settings.py

--- backend/jamco/settings.py
+++ backend/jamco/settings.py
@@ -32,7 +32,7 @@ DEBUG = not bool(os.getenv("PROD", 0))
 IS_TEST = bool(os.getenv("TEST", 1))
 PROD2 = bool(os.getenv("PROD2", 0))
 if PROD2:
-    raise ValueError("Prod shouldnt be true:" + str(PROD2) + "::" + str(os.getenv("PROD2", 0)))
+    raise ValueError("Prod shouldnt be true:" + str(PROD2) + "::" + str(os.getenv("PROD2", 0)) + "::" + str(bool(0)))
 logger.info("--------------- PROD: %s", PROD)
 
 SETTINGS_PATH = os.path.dirname(os.path.dirname(__file__))
