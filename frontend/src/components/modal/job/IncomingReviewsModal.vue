793f903cfaa69b297bf0d2df1612e4da981b112c
Alex Kitt <alex.kitt@outlook.com>
2023-03-13 14:32:22 -0500

    What the heck was I not tracking the new modal file!?

---
+++ frontend/src/components/modal/job/IncomingReviewsModal.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-row justify="center">
+    <v-dialog v-model="dialog" persistent max-width="600px">
+      <v-card>
+        <v-card-title>E</v-card-title>
+        <v-card-text>
+          <v-row>
+            <v-col>hiiiii :D</v-col>
+          </v-row>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer />
+          <v-btn
+            color="blue-darken-1"
+            variant="text"
+            @click="this.$emit('close')"
+          >
+            Close
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </v-row>
+</template>
+<script>
+export default {
+  emits: ['close'],
+  data() {
+    return {
+      dialog: true,
+    }
+  },
+}
+</script>
