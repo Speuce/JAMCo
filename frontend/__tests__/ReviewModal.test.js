482f7bd43bcc2467c059237de2563b260cbd76c2
Alex Kitt <alex.kitt@outlook.com>
2023-03-16 01:34:08 -0500

    Start tests for the review modal

---
+++ frontend/__tests__/ReviewModal.test.js
@@ -0,0 +1,24 @@
+import { mount } from '@vue/test-utils'
+import { describe, it, expect } from 'vitest';
+import ReviewModal from '../src/components/modal/job/ReviewModal.vue'
+
+describe('ReviewModal', () => {
+  let wrapper
+
+  function mountModal() {
+    wrapper = mount(ReviewModal, {})
+  }
+
+  it('emits close when close button clicked', () => {
+    mountModal()
+    let buttons = wrapper.findAllComponents({ name: 'v-btn' })
+
+    buttons.forEach((button) => {
+      if (button.text() === 'Close') {
+        button.trigger('click')
+      }
+    })
+
+    expect(wrapper.emitted('close')).toBeTruthy()
+  })
+})
