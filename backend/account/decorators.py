fbc798e9c6ba729c2510c74d443e4ca5bcaed873
Speuce <mrk5199@gmail.com>
2023-03-23 18:52:40 -0500

    Update Required Login Decorator to handle multiple cases for user validation

--- backend/account/decorators.py
+++ backend/account/decorators.py
@@ -2,21 +2,32 @@ from django.http import JsonResponse
 import logging
 
 from account.business import validate_token
+from jamco.helper import read_request
 
 logger = logging.getLogger(__name__)
 
 
-def requires_login(wrapped_view):
-    def _decorator(request, *args, **kwargs):
-        if request.COOKIES.get("auth_token"):
-            token = request.COOKIES.get("auth_token")
-            try:
-                user = validate_token(token)
-                request.user = user
-                return wrapped_view(request, *args, **kwargs)
-            except Exception as err_msg:
-                return JsonResponse(status=401, data={"error": repr(err_msg)})
-        else:
-            return JsonResponse({}, status=401)
+def requires_login(allow_friends=False, check_field="user_id"):
+    def _inner(wrapped_view):
+        def _decorator(request, *args, **kwargs):
+            if request.COOKIES.get("auth_token"):
+                token = request.COOKIES.get("auth_token")
+                try:
+                    user = validate_token(token)
+                    request.user = user
+                    body = read_request(request)
+                    if check_field is not None and body.get(check_field) != user.id:
+                        if not allow_friends:
+                            return JsonResponse({}, status=401)
+                        else:
+                            if not user.friends.filter(id=body.get(check_field)).exists():
+                                return JsonResponse({}, status=401)
+                    return wrapped_view(request, *args, **kwargs)
+                except Exception as err_msg:
+                    return JsonResponse(status=401, data={})
+            else:
+                return JsonResponse({}, status=401)
 
-    return _decorator
+        return _decorator
+
+    return _inner
