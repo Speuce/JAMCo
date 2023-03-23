f50b0923db6844a80fb47ac8432b08adf93ee6d9
Jared Webber <jaredwebberdev@gmail.com>
2023-03-23 14:58:45 -0500

    mostly ready

--- backend/tests/performance/locustfile.py
+++ backend/tests/performance/locustfile.py
@@ -3,12 +3,15 @@ from dotenv import load_dotenv
 from locust import HttpUser, task, between
 from faker import Faker
 import json
+from django.utils import timezone
 
 # Required to setup django, allowing for module import(s) below
 load_dotenv()
 django.setup()
 
-from account.models import User  # noqa: E402
+from account.models import User, FriendRequest, Privacy  # noqa: E402
+from job.models import Job  # noqa: E402
+from column.models import KanbanColumn  # noqa: E402
 
 
 # TODO: Create Test DB On Locust Launch - TestCase?
@@ -41,14 +44,14 @@ class UserActor(HttpUser):
             headers={"X-CSRFToken": self.csrftoken},
         )
         if response.status_code != 200:
-            raise Exception(f"get_or_create_account error {response.content}")
+            raise ValueError(f"get_or_create_account error {response.content}")
 
         content = json.loads(response.content)
         self.user_id = content["data"]["id"]
         self.token = content["token"]
 
         if not self.user_id:
-            raise Exception(f"No user_id in response: {content}")
+            raise ValueError(f"No user_id in response: {content}")
 
         # update account fields
         response = self.client.post(
@@ -65,7 +68,7 @@ class UserActor(HttpUser):
         )
 
         if response.status_code != 200:
-            raise Exception(f"update_account error {response.content}")
+            raise ValueError(f"update_account error {response.content}")
 
         # get user privacies
         response = self.client.post(
@@ -79,9 +82,7 @@ class UserActor(HttpUser):
         )
 
         if response.status_code != 200:
-            raise Exception(f"get_user_privacies error {response.content}")
-
-        # TODO: Create jobs for get_minimum_jobs to return?
+            raise ValueError(f"get_user_privacies error {response.content}")
 
         # get minimum jobs
         response = self.client.post(
@@ -95,7 +96,7 @@ class UserActor(HttpUser):
         )
 
         if response.status_code != 200:
-            raise Exception(f"get_minimum_jobs error {response.content}")
+            raise ValueError(f"get_minimum_jobs error {response.content}")
 
         # get user's columns
         response = self.client.post(
@@ -113,10 +114,10 @@ class UserActor(HttpUser):
         self.columns = content["columns"]
 
         if not self.columns:
-            raise Exception(f"No columns in response: {content}")
+            raise ValueError(f"No columns in response: {content}")
 
         if response.status_code != 200:
-            raise Exception(f"get_columns error {content}")
+            raise ValueError(f"get_columns error {content}")
 
     @task
     def settings_modal_interaction(self):
@@ -135,7 +136,7 @@ class UserActor(HttpUser):
         )
 
         if response.status_code != 200:
-            raise Exception(f"update_account error {response.content}")
+            raise ValueError(f"update_account error {response.content}")
 
         # update user privacy options
         response = self.client.post(
@@ -154,7 +155,7 @@ class UserActor(HttpUser):
         )
 
         if response.status_code != 200:
-            raise Exception(f"update_privacies error {response.content}")
+            raise ValueError(f"update_privacies error {response.content}")
 
     @task
     def kanban_job_interaction(self):
@@ -174,13 +175,13 @@ class UserActor(HttpUser):
         )
 
         if response.status_code != 200:
-            raise Exception(f"create_job error {response.content}")
+            raise ValueError(f"create_job error {response.content}")
 
         content = json.loads(response.content)
         job_id = content["job"]["id"]
 
         if not job_id:
-            raise Exception("No job id returned from create_job")
+            raise ValueError("No job id returned from create_job")
 
         # ~50% of the time in order to emulate non-populated kanban job
         if self.faker.boolean():
@@ -197,7 +198,7 @@ class UserActor(HttpUser):
             )
 
             if response.status_code != 200:
-                raise Exception(f"get_job_by_id error {response.content}")
+                raise ValueError(f"get_job_by_id error {response.content}")
 
         # edit job fields
         response = self.client.post(
@@ -215,29 +216,17 @@ class UserActor(HttpUser):
         )
 
         if response.status_code != 200:
-            raise Exception(f"update_job error {response.content}")
+            raise ValueError(f"update_job error {response.content}")
 
     @task
     def job_modal_request_cover_letter_review_interaction(self):
         # create job
-        response = self.client.post(
-            "/job/api/create_job",
-            json.dumps(
-                {
-                    "user_id": self.user_id,
-                    "position_title": self.faker.job(),
-                    "company": self.faker.company(),
-                    "type": self.faker.safe_color_name(),
-                    "kcolumn_id": self.columns[0]["id"],
-                }
-            ),
-            headers={"X-CSRFToken": self.csrftoken},
+        new_job = Job.objects.create(
+            position_title=self.faker.job(),
+            company=self.faker.company(),
+            user=User.objects.get(id=self.user_id),
+            kcolumn=KanbanColumn.objects.get(id=self.columns[0]["id"]),
         )
-
-        if response.status_code != 200:
-            raise Exception(f"create_job error {response.content}")
-
-        new_job_id = json.loads(response.content)["job"]["id"]
 
         # ~50% of the time in order to emulate non-populated kanban job
         if self.faker.boolean():
@@ -247,36 +236,37 @@ class UserActor(HttpUser):
                 json.dumps(
                     {
                         "user_id": self.user_id,
-                        "job_id": new_job_id,
+                        "job_id": new_job.id,
                     }
                 ),
                 headers={"X-CSRFToken": self.csrftoken},
             )
 
             if response.status_code != 200:
-                raise Exception(f"get_job_by_id error {response.content}")
+                raise ValueError(f"get_job_by_id error {response.content}")
 
         # Create Account, Add Friends
-        response = self.client.post(
-            "/account/api/get_or_create_account",
-            json.dumps({"credential": "load_test", "client_id": "load_test"}),
-            headers={"X-CSRFToken": self.csrftoken},
-        )
-        if response.status_code != 200:
-            raise Exception(f"get_or_create_account error {response.content}")
+        g_id = self.faker.uuid4()
+        new_user = User.objects.create(username=g_id, google_id=g_id)
+        User.objects.get(id=self.user_id).friends.set([new_user])
 
-        friend_user_id = json.loads(response.content)["data"]["id"]
-        User.objects.get(id=self.user_id).friends.set([User.objects.get(id=friend_user_id)])
+        # update new User privacy
+        Privacy.objects.create(
+            user=new_user,
+            is_searchable=True,
+            share_kanban=True,
+            cover_letter_requestable=True,
+        )
 
         # Send review request
         response = self.client.post(
             "/job/api/create_review_request",
-            json.dumps({"job_id": new_job_id, "reviewer_id": friend_user_id, "message": "Review Please"}),
+            json.dumps({"job_id": new_job.id, "reviewer_id": new_user.id, "message": "Review Please"}),
             headers={"X-CSRFToken": self.csrftoken},
         )
 
         if response.status_code != 200:
-            raise Exception(f"create_review_request error {response.content}")
+            raise ValueError(f"create_review_request error {response.content}")
 
     @task
     def kanban_column_interaction(self):
@@ -298,7 +288,7 @@ class UserActor(HttpUser):
         )
 
         if response.status_code != 200:
-            raise Exception(f"update_columns error {response.content}")
+            raise ValueError(f"update_columns error {response.content}")
 
     @task
     def friends_modal_request_friend_interaction(self):
@@ -310,10 +300,39 @@ class UserActor(HttpUser):
         )
 
         if response.status_code != 200:
-            raise Exception(f"get_updated_user_data error {response.content}")
+            raise ValueError(f"get_updated_user_data error {response.content}")
 
-        # TODO: search_users_by_name
-        # TODO: send_friend_request
+        # search_users_by_name
+        response = self.client.post(
+            "/account/api/search_users_by_name",
+            json.dumps("j"),
+            headers={"X-CSRFToken": self.csrftoken},
+        )
+
+        if response.status_code != 200:
+            raise ValueError(f"search_users_by_name error {response.content}")
+
+        # Create Account To Send Request To
+        g_id = self.faker.uuid4()
+        new_user = User.objects.create(username=g_id, google_id=g_id)
+
+        # update user privacy
+        Privacy.objects.create(
+            user=new_user,
+            is_searchable=True,
+            share_kanban=True,
+            cover_letter_requestable=True,
+        )
+
+        # send friend request
+        response = self.client.post(
+            "/account/api/create_friend_request",
+            json.dumps({"from_user_id": self.user_id, "to_user_id": new_user.id}),
+            headers={"X-CSRFToken": self.csrftoken},
+        )
+
+        if response.status_code != 200:
+            raise ValueError(f"create_friend_request error {response.content}")
 
     @task
     def friends_modal_friend_request_interaction(self):
@@ -325,11 +344,51 @@ class UserActor(HttpUser):
         )
 
         if response.status_code != 200:
-            raise Exception(f"get_updated_user_data error {response.content}")
+            raise ValueError(f"get_updated_user_data error {response.content}")
 
-        # TODO: accept_friend_request
-        # and/or
-        # TODO: deny_friend_request
+        # create user
+        g_id = self.faker.uuid4()
+        new_user = User.objects.create(username=g_id, google_id=g_id)
+
+        # update user privacy
+        Privacy.objects.create(
+            user=new_user,
+            is_searchable=True,
+            share_kanban=True,
+            cover_letter_requestable=True,
+        )
+
+        # add friend
+        User.objects.get(id=self.user_id).friends.set([new_user])
+
+        # create friend request
+        request = FriendRequest.objects.create(
+            from_user=User.objects.get(id=self.user_id),
+            to_user=new_user,
+            sent=timezone.now(),
+            accepted=False,
+            acknowledged=None,
+        )
+
+        # ~50/50 accept/deny request
+        if self.faker.boolean():
+            response = self.client.post(
+                "/account/api/accept_friend_request",
+                json.dumps({"request_id": request.id, "from_user_id": self.user_id, "to_user_id": new_user.id}),
+                headers={"X-CSRFToken": self.csrftoken},
+            )
+
+            if response.status_code != 200:
+                raise ValueError(f"get_updated_user_data error {response.content}")
+        else:
+            response = self.client.post(
+                "/account/api/deny_friend_request",
+                json.dumps({"request_id": request.id, "from_user_id": self.user_id, "to_user_id": new_user.id}),
+                headers={"X-CSRFToken": self.csrftoken},
+            )
+
+            if response.status_code != 200:
+                raise ValueError(f"get_updated_user_data error {response.content}")
 
     @task
     def friends_modal_friend_kanban_interaction(self):
@@ -341,9 +400,22 @@ class UserActor(HttpUser):
         )
 
         if response.status_code != 200:
-            raise Exception(f"get_updated_user_data error {response.content}")
+            raise ValueError(f"get_updated_user_data error {response.content}")
 
-        # TODO: view_friend_kanban
+        # create new user, add friends
+        g_id = self.faker.uuid4()
+        new_user = User.objects.create(username=g_id, google_id=g_id)
+        User.objects.get(id=self.user_id).friends.set([new_user])
+
+        # view friend kanban
+        response = self.client.post(
+            "/account/api/get_friend_data",
+            json.dumps({"user_id": self.user_id, "friend_id": new_user.id}),
+            headers={"X-CSRFToken": self.csrftoken},
+        )
+
+        if response.status_code != 200:
+            raise ValueError(f"get_friend_data error {response.content}")
 
     @task
     def inbox_modal_review_cover_letter_interaction(self):
@@ -355,7 +427,7 @@ class UserActor(HttpUser):
         )
 
         if response.status_code != 200:
-            raise Exception(f"get_updated_user_data error {response.content}")
+            raise ValueError(f"get_updated_user_data error {response.content}")
 
         # TODO: get_review_requests_for_user
         # TODO: get_reviews_for_user
