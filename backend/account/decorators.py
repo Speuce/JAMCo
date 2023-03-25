fdf4f6eb0f21cc1f5270c4b73cc42e28188dccb4
Speuce <mrk5199@gmail.com>
2023-03-24 19:53:17 -0500

    Refactor decorator imports to be testable

--- backend/account/decorators.py
+++ backend/account/decorators.py
@@ -1,10 +1,7 @@
 from django.http import JsonResponse
-import logging
 
-from account.business import validate_token
-from jamco.helper import read_request
-
-logger = logging.getLogger(__name__)
+import account.business as business
+import jamco.helper as helper
 
 
 def requires_login(allow_friends=False, check_field="user_id"):
@@ -13,9 +10,9 @@ def requires_login(allow_friends=False, check_field="user_id"):
             if request.COOKIES.get("auth_token"):
                 token = request.COOKIES.get("auth_token")
                 try:
-                    user = validate_token(token)
+                    user = business.validate_token(token)
                     request.user = user
-                    body = read_request(request)
+                    body = helper.read_request(request)
                     if check_field is not None and body.get(check_field) != user.id:
                         if not allow_friends:
                             return JsonResponse({}, status=401)
@@ -23,7 +20,7 @@ def requires_login(allow_friends=False, check_field="user_id"):
                             if not user.friends.filter(id=body.get(check_field)).exists():
                                 return JsonResponse({}, status=401)
                     return wrapped_view(request, *args, **kwargs)
-                except Exception as err_msg:
+                except Exception as e:
                     return JsonResponse(status=401, data={})
             else:
                 return JsonResponse({}, status=401)
